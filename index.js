186721c1ac925977daaec401db92daad8e9191b4
Ian Rapley <rapleyi@cardiff.ac.uk>
2024-10-04 09:16:15 +0100

    rework cycle

--- index.js
+++ index.js
@@ -9,33 +9,31 @@ var bsky = require("@atproto/api");
 var dotenv = require("dotenv"); 
 var cron = require("cron"); 
 var process = require("process"); 
+var express = require('express');
+var unparse = require("nearley-unparse"); //uses nearly grammar for madlibs
 
 const { BskyAgent } = bsky;
 const { CronJob } = cron;
-var unparse = require("nearley-unparse"); //uses nearly grammar for madlibs
+dotenv.config();
+
 var grammarA = require("./grammar"); // Can update the grammar to get better lists
 var grammarB = require("./Grammar-poetry.js"); 
 
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;    // Needed to ensure heroku can listen to the right port - not needed for local running
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
 
-// var dotenv = require('dotenv').config(); //needed to protect twitter api details 
-
-dotenv.config();
+// var app = express();
+// var port = process.env.PORT || 8080;    // Needed to ensure heroku can listen to the right port - not needed for local running
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
 // const port = process.env.PORT || "8080";
 
+
 // Create a Bluesky Agent 
-
-
 const agent = new BskyAgent({
     service: 'https://bsky.social',
 });
 
-
+// Basic post to Bluesky
 async function PostPillow(Pillowtext) {
 	await agent.login({
 		identifier: process.env.BLUESKY_LOGIN,
@@ -46,6 +44,8 @@ async function PostPillow(Pillowtext) {
     });
 }
 
+
+// Text generator
 function letsdothis() {
 	if (Math.random() <0.5) {
 
@@ -62,7 +62,10 @@ function letsdothis() {
 
 letsdothis;
 
-const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
-const scheduleExpression = '0 */6 * * *'; // Run once every three hours in prod
-const job = new CronJob(scheduleExpression, letsdothis);
-job.start();
+// retweet in every 5 minutes (300,000) 6 hours (21,600,000)
+setInterval(letsdothis, 300000);
+
+// const scheduleExpressionMinute = '* * * * *'; // Run once every minute for testing
+// const scheduleExpression = '0 */6 * * *'; // Run once every three hours in prod
+// const job = new CronJob(scheduleExpression, letsdothis);
+// job.start();
