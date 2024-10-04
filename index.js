e59b4ca9972ad196055caac8f25eec5410b4f9d0
Ian Rapley <rapleyi@cardiff.ac.uk>
2024-10-04 09:58:17 +0100

    environment variables take 94

--- index.js
+++ index.js
@@ -33,11 +33,14 @@ const agent = new BskyAgent({
     service: 'https://bsky.social',
 });
 
+const BSLog = process.env.BLUESKY_LOGIN
+const BSPass = process.env.BLUESKY_PASSWORD
+
 // Basic post to Bluesky
 async function PostPillow(Pillowtext) {
 	await agent.login({
-		identifier: process.env.BLUESKY_LOGIN,
-		password: process.env.BLUESKY_PASSWORD,
+		identifier: BSLog,
+		password: BSPass,
 	});
 	const response = await agent.post({
         text: Pillowtext
